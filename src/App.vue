72a606fdba8d8044b91da44957d04c63bdafb690
keyproco <keyproco.rd@gmail.com>
2023-11-11 21:33:22 +0100

    used computed property

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed} from 'vue';
   const todos = ref([
   {
     text: 'Workout',
@@ -32,24 +32,32 @@ import { ref } from 'vue';
   },
 ]);
 
+
+const completedTodos = computed(() => {
+  return todos.value.filter(todo => todo.completed)
+});
+
+const wipTodos = computed(() => {
+  return todos.value.filter(todo => ! todo.completed)
+});
 </script>
 
 <template>
   <section class="h-full grid place-items-center">
-    <div class="p-3">
+    <div v-show="wipTodos.length" class="p-3">
       <h1 class="front-bold" >Todos:</h1>
       <ul class="list-disc">
-        <li v-for="todo in todos.filter(todo => ! todo.completed)" :key="todo.id">
+        <li v-for="todo in wipTodos" :key="todo.id">
           {{ todo.text }}
           <input type="checkbox" v-model="todo.completed" :key="todo.id"/>
         </li>
       </ul>
-      
+
     </div>
-    <div class="p-3">
+    <div v-show="completedTodos.length"  class="p-3">
       <h1 class="front-bold" >Completed Tasks:</h1>
       <ul class="list-disc">
-        <li v-for="todo in todos.filter(todo => todo.completed)" :key="todo.id">
+        <li v-for="todo in completedTodos" :key="todo.id">
           {{ todo.text }}
           <input type="checkbox" v-model="todo.completed" />
         </li>
