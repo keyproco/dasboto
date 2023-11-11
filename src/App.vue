0ae8b0b298555b68e733215d7b3763c07110d025
keyproco <keyproco.rd@gmail.com>
2023-11-11 20:54:26 +0100

    listed tasks to complete

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
 <script setup>
+import { ref } from 'vue';
+  const todos = ref([
+  {
+    text: 'Workout',
+    completed: false,
+  },
+  {
+    text: 'Learn',
+    completed: false,
+  },
+  {
+    text: 'Love',
+    completed: false,
+  },
+  {
+    text: 'Work',
+    completed: false,
+  },
+  {
+    text: 'Travel',
+    completed: false,
+  },
+  {
+    text: 'Simrace',
+    completed: false,
+  },
+]);
 
 </script>
 
 <template>
-  <div class="px-10 py-5 bg-blue-600">
-    <h1>tailwind is working</h1>
-  </div>
+  <section class="h-full grid place-items-center">
+    <div class="p-3">
+      <h1 class="front-bold" >Todos:</h1>
+      <ul class="list-disc">
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.text }}
+          <input type="checkbox" v-model="todo.completed" />
+        </li>
+      </ul>
+    </div>
+    {{ todos }}
+  </section>
 </template>
 
 <style scoped>
